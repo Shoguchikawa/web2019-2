7f4409bd8a30c5c3cf4b7dbcbc9ccbd2065c52b0
zm17429 <zm17429@josai.ac.jp>
2019-01-20 20:33:57 -0500

    message

---
+++ ex6-1.js
@@ -0,0 +1,44 @@
+function showimg1(){
+  document.imgLink.src ="./image/100m.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=lHbExjyuVtE";
+  document.getElementById("message").innerHTML = "100mは100m走ります。";}
+
+function showimg2(){
+  document.imgLink.src = "./image/400m.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=xG91krXuxyw";
+  document.getElementById("message").innerHTML = "400mは400m走ります。";}
+
+function showimg3(){
+  document.imgLink.src = "./image/1500m.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=oOoYbFvftRg";
+  document.getElementById("message").innerHTML = "1500mは1500m走ります。";}
+
+function showimg4(){
+  document.imgLink.src ="./image/5000m.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=QSSBtxvd5BI";
+  document.getElementById("message").innerHTML = "5000mは5000m走ります。";}
+
+function showimg5(){
+  document.imgLink.src = "./image/3000msc.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=eGcMyHeN00E";
+  document.getElementById("message").innerHTML = "3000mscは3000を障害を乗り越えながら走ります。";}
+
+function showimg6(){
+  document.imgLink.src = "./image/棒高跳.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=C9IeqIeeppQ";
+  document.getElementById("message").innerHTML = "棒高跳は棒を使って高く跳びます。";}
+
+function showimg7(){
+  document.imgLink.src ="./image/走幅跳.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=VCjIVBEy6oI";
+  document.getElementById("message").innerHTML = "走幅跳は走って遠くまで跳びます";}
+
+function showimg8(){
+  document.imgLink.src = "./image/ハンマー投.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=yi7zLQu-DSA";
+  document.getElementById("message").innerHTML = "ハンマー投はハンマーを遠くまで投げます。";}
+
+function showimg9(){
+  document.imgLink.src = "./image/十種競技.png";
+  document.getElementById("linkArea").href = "https://www.youtube.com/watch?v=AmqiCx-6AX8";
+  document.getElementById("message").innerHTML = "十種競技は十種目行い点数を競います。";}
