53d39c36347c68143555641d962b415d364f5864
zm17429 <zm17429@josai.ac.jp>
2019-02-04 06:06:56 -0500

    a

--- hogehoge.js
+++ hogehoge.js
@@ -1,6 +1,6 @@
 var who65 ='world';
-function sayhello65(){alert('特性紹介'+ who65 +'!');}
+function sayhello65(){alert('Hello,'+ who65 +'!');}
 
-function 100m(){who65=100mは世界記録がウサインボルト;}
+function taro65(){who65='太郎';}
 
-function 200m(){who65='花子';}
+function hanako65(){who65='花子';}
