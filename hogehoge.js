fad69d7b858be817b45eb5afc7c62e554ad58c53
zm17429 <zm17429@josai.ac.jp>
2019-02-04 06:55:57 -0500

    a

--- hogehoge.js
+++ hogehoge.js
@@ -4,3 +4,24 @@ function nazo(){
   }else if(document.getElementById('answer').value=='ウサイン・ボルト'){alert('あたり！');
  }else if(document.getElementById('answer').value!=='ウサインボルト')alert('はずれ！');
 }
+
+function nazo2(){
+  if(document.getElementById('answer2').value=='ウェイド・バンニーキルク'){
+    alert('あたり！');
+  }else if(document.getElementById('answer2').value=='ウェイドバンニーキルク'){alert('あたり！');
+ }else if(document.getElementById('answer2').value!=='ウェイド・バンニーキルク')alert('はずれ！');
+}
+
+function nazo3(){
+  if(document.getElementById('answer3').value=='マイク・パウエル'){
+    alert('あたり！');
+  }else if(document.getElementById('answer3').value=='マイクパウエル'){alert('あたり！');
+ }else if(document.getElementById('answer3').value!=='マイク・パウエル')alert('はずれ！');
+}
+
+function nazo4(){
+  if(document.getElementById('answer4').value=='ルノー・ラビレニ'){
+    alert('あたり！');
+  }else if(document.getElementById('answer4').value=='ルノーラビレニ'){alert('あたり！');
+ }else if(document.getElementById('answer4').value!=='ルノー・ラビレニ')alert('はずれ！');
+}
