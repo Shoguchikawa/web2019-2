165e0a57000b42a48801a0ed461a430892613bb2
zm17429 <zm17429@josai.ac.jp>
2019-02-04 06:21:11 -0500

    a

--- hogehoge.js
+++ hogehoge.js
@@ -1,7 +1,7 @@
 var who65 ='world';
 function sayhello65(){alert('Hello,'+ who65 +'!');}
 
-function taro65(){who65='太郎';}
+function taro65(){who65='100メートルをいかに短い時間で走るかを競う陸上競技で、短距離走に分類される。世界記録は9秒58';}
 
 function hanako65(){who65='花子';}
 
