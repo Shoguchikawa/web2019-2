5265a46caf77c26efd7673e91a9de9223d5994b8
zm17429 <zm17429@josai.ac.jp>
2019-02-04 06:47:56 -0500

    a

--- hogehoge.js
+++ hogehoge.js
@@ -1,6 +1,6 @@
 function nazo(){
   if(document.getElementById('answer').value=='ウサインボルト'){
     alert('あたり！');
-  }else if(document.getElementById('answer2').value=='ウサイン・ボルト'){alert('あたり！');
- }else if(document.getElementById('answer2').value!=='ウサインボルト')alert('はずれ！');
+  }else if(document.getElementById('answer').value=='ウサイン・ボルト'){alert('あたり！');
+ }else if(document.getElementById('answer').value!=='ウサインボルト')alert('はずれ！');
 }
