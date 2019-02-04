be283497878a60ee5c023b4a15461276f5b6587e
zm17429 <zm17429@josai.ac.jp>
2019-02-04 06:38:31 -0500

    s

--- hogehoge.js
+++ hogehoge.js
@@ -1,8 +1,6 @@
-var who65 ='world';
-function sayhello65(){alert('Hello,'+ who65 +'!');}
-
-function taro65(){who65='100メートルをいかに短い時間で走るかを競う陸上競技で、短距離走に分類される。世界記録は9秒58';}
-
-function hanako65(){who65='花小';}
-
-function hana65(){who65='花';}
+function nazo1(){
+  if(document.getElementById('answer').value=='ウサインボルト'){
+    alert('あたり！');
+  }else if(document.getElementById('answer2').value=='ウサイン・ボルト'){alert('あたり！');
+ }else if(document.getElementById('answer2').value!=='ウサインボルト')alert('はずれ！');
+}
