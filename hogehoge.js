a641cd1a7e13eca1cf05042c4010f2b8e22d45c4
zm17429 <zm17429@josai.ac.jp>
2019-02-04 06:41:59 -0500

    f

--- hogehoge.js
+++ hogehoge.js
@@ -1,4 +1,4 @@
-function nazo1(){
+function nazo(){
   if(document.getElementById('answer').value=='ウサインボルト'){
     alert('あたり！');
   }else if(document.getElementById('answer2').value=='ウサイン・ボルト'){alert('あたり！');
